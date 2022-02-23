d61996db9aa3b83ff488a0a705ec24608c4a15d0
Mitch Andrews <themitchellandrews@gmail.com>
2022-02-22 17:34:28 -0700

    Added More Info To Saved .json file for images

--- src/catgirl_bot.py
+++ src/catgirl_bot.py
@@ -11,21 +11,24 @@ class bot:
         newText = text.stoic.uwuStoic()
         newPhoto = catgirl.getCatgirlImage(catgirl.getCatgirlJson(), "catgirl")
         newPhoto["text"] = newText
-        # save newphoto json to file
-        with open(os.path.join("catgirl", os.path.splitext(os.path.basename(newPhoto["url"]))[0] + ".json"), 'w') as f:
-            json.dump(newPhoto, f, indent=4, ensure_ascii=False)
 
         photoPath = newPhoto["save_path"]
         photoAuthor = newPhoto["artist_name"]
 
-        uwuPic = image(photoPath)
-        uwuPic.addText(newText)
         basename = os.path.basename(photoPath)
         savePath = os.path.join("catgirl", os.path.splitext(basename)[0] + "_uwu" + os.path.splitext(basename)[1])
+        newPhoto["uwu_path"] = savePath
+
+        # save newphoto dictionary to the json file
+        with open(os.path.join("catgirl", os.path.splitext(os.path.basename(newPhoto["url"]))[0] + ".json"), 'w') as f:
+            json.dump(newPhoto, f, indent=4, ensure_ascii=False)
+
+        uwuPic = image(photoPath)
+        uwuPic.addText(newText)
         uwuPic.save(savePath)
+
         self.author = photoAuthor
         self.path = savePath
-        return savePath, photoAuthor
 
 if __name__ == "__main__":
     newCatgirl = bot()
