be3ac91a57725480c3961b419961617eecae4882
Mitch Andrews <themitchellandrews@gmail.com>
2022-02-28 08:58:51 -0700

    Added Scheduling / Every 2 Hours

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from src import catgirl_bot
+import schedule as sch
+import time
 
-print("Starting Catgirl Bot...")
-catgirl_bot.newStoicCatgirlPost()
-print("Catgirl Bot Finished.")
+def main():
+    currentTime = time.localtime()
+    print("Starting Catgirl Bot at " + str(currentTime.tm_hour) + ":" + str(currentTime.tm_min))
+    catgirl_bot.newStoicCatgirlPost()
+    print("Catgirl Bot Finished.")
+
+if __name__ == "__main__":
+    sch.every(2).hours.do(main)  # Runs Main Every 2 Hours
+    while True:
+        sch.run_pending()
+        time.sleep(1)
