7456551cc0c6838386df376fc1d5f6a291bd746e
Mitch Andrews <themitchellandrews@gmail.com>
2022-02-28 09:43:17 -0700

    Dockerized!

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 
 def main():
     currentTime = time.localtime()
-    print("Starting Catgirl Bot at " + str(currentTime.tm_hour) + ":" + str(currentTime.tm_min))
+    print("Starting Posting at " + str(currentTime.tm_hour) + ":" + str(currentTime.tm_min))
     catgirl_bot.newStoicCatgirlPost()
-    print("Catgirl Bot Finished.")
+    print("Catgirl Bot Posted To Twitter.")
 
 if __name__ == "__main__":
+    print("Starting Catgirl Bot")
     sch.every(2).hours.do(main)  # Runs Main Every 2 Hours
+    # sch.every(1).minutes.do(main)  # Runs Main Every 1 Minute
     while True:
         sch.run_pending()
         time.sleep(1)
